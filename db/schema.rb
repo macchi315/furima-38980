9b893002d8d672caa64d0db00fbd1d65148002a6
macchi315 <machidamitsutaka315@gmail.com>
2023-03-08 16:48:23 +0900

    出品ページ表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_06_083427) do
+ActiveRecord::Schema.define(version: 2023_03_08_071057) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "price", null: false
+    t.string "item_name", null: false
+    t.text "item_explanation", null: false
+    t.integer "category_id", null: false
+    t.integer "description_id", null: false
+    t.integer "charge_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "day_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2023_03_06_083427) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
