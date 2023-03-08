5b1749914e0498ed14a6a390b39543a6556aac16
macchi315 <machidamitsutaka315@gmail.com>
2023-03-09 00:46:18 +0900

    ActiveHashとバリデーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,23 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    #@item = item.new
+  end
+
+  def create
+    @item = item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    item.require(:item).permit(:item_name,:item_explanation,:category_id,:description_id,:charge_id,:prefecture_id,:day_id, :image)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,27 @@
 class Item < ApplicationRecord
+
+  validates :image, presence: true
+  validates :price, presence: true, numericality: { in: 300..9999999 }, format: { with: /\d/ }
+  validates :item_name, presence: true
+  validates :item_explanation, presence: true
+  validates :category_id, presence: true
+  validates :description_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :charge_id, presence: true
+  validates :day_id , presence: true
+  validates :category_id, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :description
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :day
+  has_one_attached :image
+  validates :item_name, :item_explanation, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :description_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
