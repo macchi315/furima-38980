499a2f4f82a4c772ff3acc0e15ab4e80f5c5d35b
macchi315 <machidamitsutaka315@gmail.com>
2023-03-14 22:06:28 +0900

    提出後修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_login, only: [:new, :edit, :destroy]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :move_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -53,8 +53,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
-    return if user_signed_in? && current_user.id == @item.user_id
+    return if current_user.id == @item.user_id
     
     redirect_to action: :index
   end
