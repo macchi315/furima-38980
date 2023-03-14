00b7217148871587525be930d9b24c1151912bbf
macchi315 <machidamitsutaka315@gmail.com>
2023-03-14 20:51:22 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_login, only: [:new, :edit]
   before_action :move_to_index, only: [:edit, :update]
-  before_action :move_item, only: [:show, :edit, :update]
+  before_action :move_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -32,6 +32,10 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
   end
 
   private
