dcdc70995db2d1593b4c80762192fbc6cb6ed478
macchi315 <machidamitsutaka315@gmail.com>
2023-03-18 12:50:57 +0900

    カード機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,6 +12,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_payment = OrderPayment.new(order_params)
     if @order_payment.valid?
+      pay_item
       @order_payment.save
       redirect_to root_path
     else
@@ -22,15 +23,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_payment).permit(:item_id, :postcode, :prefecture_id, :sityouson, :banchi, :build_name, :tell_number).merge(user_id: current_user.id, item_id: params[:item_id])
-    #params.require(:order_payment).permit(:postcode, :prefecture_id, :sityouson, :banchi, :build_name, :tell_number)
+    params.require(:order_payment).permit(:item_id, :postcode, :prefecture_id, :sityouson, :banchi, :build_name, :tell_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
-
-#:postcode,
-#:prefecture_id,
-#:sityouson,
-#:banchi,
-#:build_name
-#:tell_number
-#:item_name, :nickname, :day_id,
